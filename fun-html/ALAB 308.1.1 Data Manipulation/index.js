49f84d9ba1fafaf063a821595940311decb01571
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-08-11 10:03:11 -0500

    Changed JS Pract folder to lab title. Also changed isLessThan25 variable to still validate with new initialization

--- fun-html/ALAB 308.1.1 Data Manipulation/index.js
+++ fun-html/ALAB 308.1.1 Data Manipulation/index.js
@@ -8,37 +8,37 @@ const n2 = 15;
 const n3 = 20;
 const n4 = 5;
 
-// // Check one: add up to 50
-// // This is a fairly simple operation using
-// // arithmetic operators and a comparison.
-// const isSum50 = (n1 + n2 + n3 + n4) == 50;
+// Check one: add up to 50
+// This is a fairly simple operation using
+// arithmetic operators and a comparison.
+const isSum50 = (n1 + n2 + n3 + n4) == 50;
 
-// // Check two: at least two odd numbers
-// // Here, we use modulus to check if something is odd.
-// // Since % 2 is 0 if even and 1 if odd, we can use
-// // arithmetic to count the total number of odd numbers.
-// const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+// Check two: at least two odd numbers
+// Here, we use modulus to check if something is odd.
+// Since % 2 is 0 if even and 1 if odd, we can use
+// arithmetic to count the total number of odd numbers.
+const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 
-// // Check three: no number larger than 25
-// // This time, we use the OR operator to check
-// // if ANY of the numbers is larger than 25.
+// Check three: no number larger than 25
+// This time, we use the OR operator to check
+// if ANY of the numbers is larger than 25.
 // const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
-// // const isLessThan25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
+const isLessThan25 = (n1 && n2 && n3 && n4) < 25;
 
-// // Check four: all unique numbers
-// // This is long, and there are more efficient
-// // ways of handling it with other data structures
-// // that we will review later.
-// const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+// Check four: all unique numbers
+// This is long, and there are more efficient
+// ways of handling it with other data structures
+// that we will review later.
+const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
 
-// // Here, we put the results into a single variable 
-// // for convenience. Note how we negate isOver25 using
-// // the ! operator. We could also have tested for 
-// // "isUnder25" as an alternative.
-// const isValid = isSum50 && isTwoOdd && isLessThan25 && isUnique;
+// Here, we put the results into a single variable 
+// for convenience. Note how we negate isOver25 using
+// the ! operator. We could also have tested for 
+// "isUnder25" as an alternative.
+const isValid = isSum50 && isTwoOdd && isLessThan25 && isUnique;
 
-// // Finally, log the results.
-// console.log(isValid);
+// Finally, log the results.
+console.log(isValid);
 
 // // Here's another example of how this COULD be done,
 // // but it SHOULD NOT be done this way. As programmers,
@@ -54,7 +54,7 @@ const n4 = 5;
     // Part 1: Math Problems
 
   //Check if all numbers are divisible by 5. Cache the result in a variable.
-  const divisibleBy5 = ((n1 && n2 && n3 && n4)%5 == 0);
+  const divisibleBy5 = ((n1 && n2 && n3 && n4)%5 === 0);
   console.log(divisibleBy5);
 
   // Check if the first number is larger than the last. Cache the result in a variable.
@@ -69,7 +69,8 @@ const n4 = 5;
   console.log(arithmeticChain);
 
   // Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
-  const isLessThan25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
+  //used '_isLessThan25' to not interfere with line 26 to confirm validation test
+  const _isLessThan25 = (n1 && n2 && n3 && n4) < 25;
 
 
     //Part 2: Practical Math
