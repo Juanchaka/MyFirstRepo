44b8f5b77fe37440aee449b8c54ee7af591ec7ac
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-08-17 10:51:28 -0500

    trying to create a function to add the elements more efficiently so they can be appended in one step, not multiple

--- fun-html/DOMPract/index.js
+++ fun-html/DOMPract/index.js
@@ -1,25 +1,32 @@
-const containerDiv = Object.assign(document.createElement('div'),
-{className:'container'});
-const headerDiv = Object.assign(document.createElement('div'), {className:'header'});
-const h2Element = Object.assign(document.createElement('h2'),
-{innerHTML: "Quote Generator"});
-const mainContentDiv = Object.assign(document.createElement('div'), {className: 'main-content'});
-const textAreaContent = Object.assign(document.createElement('div'),
-{className:'text-area'});
-const quoteSpan = Object.assign(document.createElement('span'),
-{className: 'quote'}, {innerHTML: ` "I try to do my best, but sometimes my best isn't good emough!"`});
-const person = Object.assign(document.createElement('div'), {className: 'person'}, {innerHTML: "James Bond"})
-const buttonArea = Object.assign(document.createElement('div'),
-{className: 'button-area'});
-const buttonElement = Object.assign(document.createElement('button'),
-{id:'new-quote'}, {innerText: "New Quote"});
+const createElement = (tag, attributes = {}, ...children) => {
+    const element = document.createElement(tag);
+    Object.assign(element, attributes);
+    children.forEach(child => element.appendChild(child));
+    return element;
+  };
 
-containerDiv.appendChild(headerDiv);
-headerDiv.appendChild(h2Element);
-containerDiv.appendChild(mainContentDiv);
-mainContentDiv.appendChild(textAreaContent);
-textAreaContent.appendChild(quoteSpan);
-mainContentDiv.appendChild(person);
-buttonArea.appendChild(buttonElement);
-mainContentDiv.appendChild(buttonArea);
-document.body.appendChild(containerDiv);
+// const containerDiv = Object.assign(document.createElement('div'),
+// {className:'container'});
+// const headerDiv = Object.assign(document.createElement('div'), {className:'header'});
+// const h2Element = Object.assign(document.createElement('h2'),
+// {innerHTML: "Quote Generator"});
+// const mainContentDiv = Object.assign(document.createElement('div'), {className: 'main-content'});
+// const textAreaContent = Object.assign(document.createElement('div'),
+// {className:'text-area'});
+// const quoteSpan = Object.assign(document.createElement('span'),
+// {className: 'quote'}, {innerHTML: ` "I try to do my best, but sometimes my best isn't good emough!"`});
+// const person = Object.assign(document.createElement('div'), {className: 'person'}, {innerHTML: "James Bond"})
+// const buttonArea = Object.assign(document.createElement('div'),
+// {className: 'button-area'});
+// const buttonElement = Object.assign(document.createElement('button'),
+// {id:'new-quote'}, {innerText: "New Quote"});
+
+// containerDiv.appendChild(headerDiv);
+// headerDiv.appendChild(h2Element);
+// containerDiv.appendChild(mainContentDiv);
+// mainContentDiv.appendChild(textAreaContent);
+// textAreaContent.appendChild(quoteSpan);
+// mainContentDiv.appendChild(person);
+// buttonArea.appendChild(buttonElement);
+// mainContentDiv.appendChild(buttonArea);
+// document.body.appendChild(containerDiv);
