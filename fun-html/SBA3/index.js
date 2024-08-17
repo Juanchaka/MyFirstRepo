fe16ff830f0306d78c3f670c975cf8ad3b2693c7
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-08-17 13:19:42 -0500

    the SBA is complete and working. submitting current link as finished product, but will add to make sure all requirements outlined are met

--- fun-html/SBA3/index.js
+++ fun-html/SBA3/index.js
@@ -112,37 +112,4 @@ headerContent.addEventListener('mouseover', function(){
 
 headerContent.addEventListener('mouseout', function(){
   headerContent.style.boxShadow = "none";
-})
-
-//2nd event
-//hover over main content and header and make the event provide a box-shadow: 0 0 10px rgb(202, 86, 86);
-
-//3rd event
-//test if the button active state can be changed instead of using CSS, but not necessary
-
-//OLD code to create elements and then append each element to the parent and then to the body - refactored to complete steps in a function then return the elements and finally append everything in one step.
-// const containerDiv = Object.assign(document.createElement('div'),
-// {className:'container'});
-// const headerDiv = Object.assign(document.createElement('div'), {className:'header'});
-// const h2Element = Object.assign(document.createElement('h2'),
-// {innerHTML: "Quote Generator"});
-// const mainContentDiv = Object.assign(document.createElement('div'), {className: 'main-content'});
-// const textAreaContent = Object.assign(document.createElement('div'),
-// {className:'text-area'});
-// const quoteSpan = Object.assign(document.createElement('span'),
-// {className: 'quote'}, {innerHTML: ` "I try to do my best, but sometimes my best isn't good emough!"`});
-// const person = Object.assign(document.createElement('div'), {className: 'person'}, {innerHTML: "James Bond"})
-// const buttonArea = Object.assign(document.createElement('div'),
-// {className: 'button-area'});
-// const buttonElement = Object.assign(document.createElement('button'),
-// {id:'new-quote'}, {innerText: "New Quote"});
-
-// containerDiv.appendChild(headerDiv);
-// headerDiv.appendChild(h2Element);
-// containerDiv.appendChild(mainContentDiv);
-// mainContentDiv.appendChild(textAreaContent);
-// textAreaContent.appendChild(quoteSpan);
-// mainContentDiv.appendChild(person);
-// buttonArea.appendChild(buttonElement);
-// mainContentDiv.appendChild(buttonArea);
-// document.body.appendChild(containerDiv);
+});
